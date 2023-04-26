ae761de11825422e2339b71ad72e8cba73a39340
SvoboduPopugau <Vlad42410@gmail.com>
2023-04-26 15:30:47 +0300

    Part 2 completed

--- src/main/java/Topwar/SiteFetcher/Main.java
+++ src/main/java/Topwar/SiteFetcher/Main.java
@@ -1,20 +1,47 @@
 package Topwar.SiteFetcher;
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.jsoup.nodes.Document;
 
+import java.util.Collection;
+import java.util.Properties;
+
 public class Main {
     private static Logger log = LogManager.getLogger();
     private static String site = "https://topwar.ru/news/";
+    private static Thread taskController, taskConsumer;
 
     public static void main(String[] args) {
-        TaskController taskController = new TaskController(site);
-        Document doc = taskController.getUrl(site);
-        String title;
-        if (doc != null) {
-            title = doc.title();
-            log.info(title);
-            taskController.ParseNews(doc);
+        Properties props = System.getProperties();
+//        TaskController taskController = new TaskController(site);
+//        Document doc = taskController.getUrl(site);
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setUsername("rabbitmq");
+        factory.setPassword("rabbitmq");
+        factory.setVirtualHost("/");
+        factory.setHost("127.0.0.1");
+        factory.setPort(5672);
+
+        try {
+            Connection conn = factory.newConnection();
+            Channel channel = conn.createChannel();
+            taskController = new Thread(new TaskController(channel, site));
+            taskController.start();
+            taskConsumer = new Thread(new TaskController(channel, null));
+            taskConsumer.start();
+        } catch (Exception ex) {
+            log.error(ex);
+            return;
         }
+
+//        String title;
+//        if (doc != null) {
+//            title = doc.title();
+//            log.info(title);
+//            taskController.ParseNews(doc);
+//        }
     }
 }
--- src/main/java/Topwar/SiteFetcher/TaskController.java
+++ src/main/java/Topwar/SiteFetcher/TaskController.java
@@ -1,44 +1,76 @@
 package Topwar.SiteFetcher;
 
+import com.rabbitmq.client.*;
 import org.apache.http.HttpEntity;
 import org.apache.http.client.CookieStore;
 import org.apache.http.client.config.RequestConfig;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.BasicCookieStore;
-import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.json.simple.JSONObject;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
+import com.rabbitmq.client.Channel;
 
-import javax.print.Doc;
-import java.io.*;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.util.Objects;
 
-import org.json.simple.JSONObject;
 
-
-public class TaskController {
+public class TaskController extends Thread{
     private static Logger log = LogManager.getLogger();
+    static String exchangeName = "";
+    static String queueUrl = "start_urls";
+    static String exchangeName_1 = "";
+    static String queuePosts = "parsed_posts";
     private CloseableHttpClient client = null;
     private HttpClientBuilder builder;
     private String server = "https://topwar.ru/news/";
     private int retryDelay = 5 * 1000;
     private int retryCount = 2;
     private int metadataTimeout = 30 * 1000;
+    private Channel channel;
 
-    public TaskController(String _server) {
+    public TaskController(Channel channel, String _server) {
+        this.channel = channel;
         CookieStore httpCookiesStore = new BasicCookieStore();
         builder = HttpClientBuilder.create().setDefaultCookieStore(httpCookiesStore);
         client = builder.build();
         this.server = _server;
+
+    }
+    @Override
+    public void run(){
+        if (server != null) {
+            ParseNews(getUrl(server));
+        } else {
+            getPage();
+        }
     }
 
-    public  Document getUrl(String url) {
+    public void publishToMQ (String message, String exName, String queueName) {
+        byte [] messageBodyBytes = message.getBytes();
+        try {
+            channel.basicPublish(
+                    exName,
+                    queueName,
+                    false,
+                    MessageProperties.PERSISTENT_TEXT_PLAIN,
+                    messageBodyBytes
+            );
+        } catch (Exception ex) {
+            log.error(ex);
+        }
+    }
+
+//    Скачивает html-страницу и парсит в jsoup.document
+    public Document getUrl(String url) {
         int code = 0;
         boolean bStop = false;
         Document doc = null;
@@ -106,6 +138,7 @@ public class TaskController {
         return doc;
     }
 
+//    Собираем все ссылки с начальной страницы и отправляем в очередь
     public void ParseNews(Document doc) {
         try {
             Elements news = doc.select("#dle-content > article > div.post-cont > h2 > a");
@@ -113,13 +146,14 @@ public class TaskController {
                 try {
                     String link = element.attr("href");
                     log.info(element.text());
-//                    TODO: Добавить очередь MQ
-                    JSONObject newsData = getPage(link);
-                    if (newsData != null){
-                        log.info(newsData);
-                    } else {
-                        log.warn("Страница " + link + " пуста");
-                    }
+
+                    publishToMQ(link, exchangeName, queueUrl);
+//                    JSONObject newsData = getPage(link);
+//                    if (newsData != null){
+//                        log.info(newsData);
+//                    } else {
+//                        log.warn("Страница " + link + " пуста");
+//                    }
                 } catch (Exception e) {
                     log.error(e);
                 }
@@ -129,23 +163,46 @@ public class TaskController {
         }
     }
 
-    public JSONObject getPage(String link) {
-        Document ndoc = getUrl(link);
-        JSONObject file = new JSONObject();
-        String title = null;
-        String datetime = null;
-        String text = null;
-        if (ndoc != null) {
-            title = Objects.requireNonNull(ndoc.selectFirst("#full-story > article > h1")).text();
-            datetime = Objects.requireNonNull(ndoc.selectFirst("#full-story > article > div.meta.fs-0875.c-muted.fw-b > time")).attr("datetime");
-            Elements textelements = ndoc.getElementsByClass("pfull-cont text");
-            text = textelements.text();
+    public void getPage() {
+        try {
+            channel.basicConsume(queueUrl, false, "ConsumerTag", new DefaultConsumer(channel) {
+                @Override
+                public void handleDelivery(String consumerTag,
+                                           Envelope envelope,
+                                           AMQP.BasicProperties properties,
+                                           byte[] body)
+                        throws IOException {
+                    long deliveryTag = envelope.getDeliveryTag();
+                    String link = new String(body, StandardCharsets.UTF_8);
+                    log.info(link);
 
-            file.put("url", link);
-            file.put("datetime", datetime);
-            file.put("title", title);
-            file.put("text", text);
+//                    Document ndoc = getUrl(link);
+                    Document ndoc = Jsoup.connect(link).get();
+                    JSONObject file = new JSONObject();
+                    String title = null;
+                    String datetime = null;
+                    String text = null;
+                    if (ndoc != null) {
+                        title = Objects.requireNonNull(ndoc.selectFirst("#full-story > article > h1")).text();
+                        datetime = Objects.requireNonNull(ndoc.selectFirst("#full-story > article > div.meta.fs-0875.c-muted.fw-b > time")).attr("datetime");
+                        Elements textelements = ndoc.getElementsByClass("pfull-cont text");
+                        text = textelements.text();
+
+                        file.put("url", link);
+                        file.put("datetime", datetime);
+                        file.put("title", title);
+                        file.put("text", text);
+                    }
+                    log.info("Инфорамция о статье" + file.toString());
+//                    TODO: Добавить в очередь где будут результаты парсинга статьи
+                    publishToMQ(file.toString(), exchangeName_1, queuePosts);
+                    channel.basicAck(deliveryTag, false);
+                }
+            });
+        } catch (IOException e) {
+            log.error(e);
         }
-        return file;
+
+
     }
 }
